078137fb593f59ce56b1aa6c004694c82e85ef39
Chaya Deaver <chayadeaver@gmail.com>
2019-09-12 00:32:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,12 @@ def oxford_comma(array)
 basket = array.length
 
   case basket
-  when basket = 1
+  when basket == 1
     array.join
   when basket == 2
     array[1] << " and "
     array.join
-  when basket > 3
+  when basket > 3 
     array[-1].prepend "and "
     array.join(", ")
   end
