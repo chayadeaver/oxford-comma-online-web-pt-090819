3dbfcdd5ecf7d6aad138fba69c26e8a55042b882
Chaya Deaver <chayadeaver@gmail.com>
2019-09-12 00:41:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  basket = array.length
+  if basket == 2
+    array[-2] << " and "
+    array.join
+  elsif basket == 1
+    array.join
+  elsif basket > 2
+    array[-1].prepend "and "
+    array.join(", ")
+  end
 end
